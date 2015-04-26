affb2588c3cfdd23082b2b09f42accd86e161737
zpanderson <zpanderson88@gmail.com>
2015-04-26 17:42:05 -0500

    Still trying to fix re-directs....

--- wpscripts/nav_12tree.js
+++ wpscripts/nav_12tree.js
@@ -16,20 +16,15 @@ var nav_12tree = WpNavBar.readTree({
         'bIsExternal':true,
         'sUrl':'http://schneiderhall.com/m.gallery.html'
     },
-    {   'sTitle':'Services',
+    {   'sTitle':'Wedding Packages',
         'bIsExternal':true,
-        'sUrl':'',
-        'sTarget':'_self',"childArray" : [
-        {   'sTitle':'Wedding Packages',
-            'bIsExternal':true,
-            'sUrl':'http://schneiderhall.com/m.wedding_packages.html',
-            'sTarget':'_self'
-        },
-        {   'sTitle':'Party Packages',
-            'bIsExternal':true,
-            'sUrl':'http://schneiderhall.com/m.party_packages.html',
-            'sTarget':'_self'
-        }]
+        'sUrl':'http://schneiderhall.com/m.wedding_packages.html',
+        'sTarget':'_self'
+    },
+    {   'sTitle':'Party Packages',
+        'bIsExternal':true,
+        'sUrl':'http://schneiderhall.com/m.party_packages.html',
+        'sTarget':'_self'
     },
     {   'sTitle':'Calendar',
         'bIsExternal':true,
