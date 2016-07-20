d9228bdcca52460a44600a973e632c58c489e2a5
zpanderson <zpanderson88@gmail.com>
2016-07-20 00:34:18 -0500

    Updated package prices (mobile)

--- wpscripts/nav_12tree.js
+++ wpscripts/nav_12tree.js
@@ -6,34 +6,45 @@ var nav_12tree = WpNavBar.readTree({
     'sTarget':'_self',"childArray" : [
     {   'sTitle':'Home',
         'bIsExternal':true,
-        'sUrl':'http://schneiderhall.com/m.index.html'
+        'bIsWebPath':true,
+        'sUrl':'m.index.html'
     },
     {   'sTitle':'About Us',
         'bIsExternal':true,
-        'sUrl':'http://schneiderhall.com/m.about_us.html'
+        'bIsWebPath':true,
+        'sUrl':'m.about_us.html'
     },
     {   'sTitle':'Gallery',
         'bIsExternal':true,
-        'sUrl':'http://schneiderhall.com/m.gallery.html'
+        'bIsWebPath':true,
+        'sUrl':'m.gallery.html'
     },
-    {   'sTitle':'Wedding Packages',
+    {   'sTitle':'Services',
         'bIsExternal':true,
-        'sUrl':'http://schneiderhall.com/m.wedding_packages.html',
-        'sTarget':'_self'
-    },
-    {   'sTitle':'Party Packages',
-        'bIsExternal':true,
-        'sUrl':'http://schneiderhall.com/m.party_packages.html',
-        'sTarget':'_self'
+        'sUrl':'',"childArray" : [
+        {   'sTitle':'Wedding Packages',
+            'bIsExternal':true,
+            'bIsWebPath':true,
+            'sUrl':'m.wedding_packages.html',
+            'sTarget':'_self'
+        },
+        {   'sTitle':'Party Packages',
+            'bIsExternal':true,
+            'bIsWebPath':true,
+            'sUrl':'m.party_packages.html',
+            'sTarget':'_self'
+        }]
     },
     {   'sTitle':'Calendar',
         'bIsExternal':true,
-        'sUrl':'http://schneiderhall.com/m.calendar.html',
+        'bIsWebPath':true,
+        'sUrl':'m.calendar.html',
         'sTarget':'_self'
     },
     {   'sTitle':'Contact Us',
         'bIsExternal':true,
-        'sUrl':'http://schneiderhall.com/m.contact_us.html'
+        'bIsWebPath':true,
+        'sUrl':'m.contact_us.html'
     }]
 }]
 });
