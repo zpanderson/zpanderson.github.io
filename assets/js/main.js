c0fbbead46ec2c6184c1756da6074f0799e8978c
zach <zachary.anderson@us.abb.com>
2017-05-12 11:26:02 -0500

    Revert "Trying out new gallery"
    This reverts commit 184dfa4e7efa5d7af9e9e3fce8fe6decf9ee47cf.

--- assets/js/main.js
+++ assets/js/main.js
@@ -310,39 +310,6 @@
 						});
 
 				});
-        
-            // Gallery.
-                $window.on('load', function() {
-
-                    var $gallery = $('.gallery');
-
-                    $gallery.poptrox({
-                        baseZIndex: 10001,
-                        useBodyOverflow: false,
-                        usePopupEasyClose: false,
-                        overlayColor: '#1f2328',
-                        overlayOpacity: 0.65,
-                        usePopupDefaultStyling: false,
-                        usePopupCaption: true,
-                        popupLoaderText: '',
-                        windowMargin: 50,
-                        usePopupNav: true
-                    });
-
-                    // Hack: Adjust margins when 'small' activates.
-                        skel
-                            .on('-small', function() {
-                                $gallery.each(function() {
-                                    $(this)[0]._poptrox.windowMargin = 50;
-                                });
-                            })
-                            .on('+small', function() {
-                                $gallery.each(function() {
-                                    $(this)[0]._poptrox.windowMargin = 5;
-                                });
-                            });
-
-                });
 
 			// Events.
 				$body.on('click', function(event) {
